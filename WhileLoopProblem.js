cbb4c7996577c6bfb300c6cbeae0f8dc1976e205
TanmayJain97 <mail.tanmay.jain@gmail.com>
2020-11-06 21:40:51 +0530

    Completed Day 32 Practice Problem

--- WhileLoopProblem.js
+++ WhileLoopProblem.js
@@ -18,9 +18,22 @@ a. Ask the user to think of a number n between 1 to 100
 b. Then check with the user if the number is less then n/2 or greater
 c. Repeat till the Magic Number is reached.. 
 */
-function getMagicNum(num){
-    
-}
+let max=100;
+let min=1;
+let num=Math.floor(Math.random()*100)+1;
+console.log("Generated number: "+num);
+let mid=50;
+do{
+    mid=Math.floor(((min+max)/2));
+    if(num<mid){
+        max=mid;
+        console.log(num+" is less than "+mid);
+    }else if(num>mid){
+        min=mid;
+        console.log(num+" is more than "+mid);
+    }
+}while(num!=mid);
+console.log("Found "+mid);
 
 //Coin flip till one appears 9 times
 let headCount=0;
